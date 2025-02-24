7d31cf8e2ac3823cf9f1cecbfd6948ef29a672b9
meghaa04 <161616100+meghaa04@users.noreply.github.com>
2025-02-24 21:14:14 +0530

    chanes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "../App.css";
 
 function Home() {
-  return <h1 className="title">Welcome to Smart City Management</h1>;
+  return (
+    <div className="home-container">
+      <video autoplay muted loop plays-inline class="background-video">
+        <source src="background.mp4" type="video/mp4" />
+      </video>
+
+      {/* Title */}
+      <h1 className="title">Welcome to Smart City Management</h1>
+    </div>
+  );
 }
 
 export default Home;
